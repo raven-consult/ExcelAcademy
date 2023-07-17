8fc70122ed32da5d5c4f769fe30f8a67eefe8ec6
Damian Akpan <dami.akpan@gmail.com>
2023-07-17 19:34:23 +0100

    [WIP]: Courtesy push

--- lib/pages/home/main/ongoing_course.dart
+++ lib/pages/home/main/ongoing_course.dart
@@ -1,3 +1,5 @@
+import "dart:math" as math;
+
 import "package:flutter/material.dart";
 
 class OngoingCourse extends StatelessWidget {
@@ -5,62 +7,101 @@ class OngoingCourse extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width - 24;
     return InkWell(
       splashColor: Colors.blue,
       splashFactory: InkSplash.splashFactory,
       onTap: () {
         print("Ongoing course");
       },
-      child: Container(
-        height: 165,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          borderRadius: BorderRadius.circular(4),
-        ),
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: SizedBox(
+        height: 175,
+        child: Stack(
+          clipBehavior: Clip.antiAlias,
           children: [
-            Text(
-              "Ongoing course",
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Text(
-              "ICAN Public Sector Accounting and Finance (Revision)",
-              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: Container(
-                    height: 4,
-                    width: double.infinity,
-                    color: Colors.blue,
+            Positioned(
+              bottom: 0,
+              width: width,
+              height: 175,
+              child: Transform.rotate(
+                angle: -math.pi * 0.025,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(4),
                   ),
                 ),
-                const SizedBox(width: 8),
-                const Text("23% of 100%"),
-              ],
+              ),
             ),
-            Column(
-              children: [
-                Row(
+            Positioned(
+              bottom: 0,
+              height: 175,
+              width: width,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white70,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      "Continue learning",
-                      style: Theme.of(context).textTheme.titleMedium,
+                      "Ongoing course",
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
-                    const Icon(Icons.chevron_right),
+                    Text(
+                      "ICAN Public Sector Accounting and Finance (Revision)",
+                      style:
+                          Theme.of(context).textTheme.headlineSmall?.copyWith(
+                                fontWeight: FontWeight.bold,
+                              ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          child: Stack(
+                            children: [
+                              Container(
+                                height: 4,
+                                width: MediaQuery.of(context).size.width - 3,
+                                color: Colors.blue,
+                              ),
+                              Container(
+                                height: 4,
+                                width: ((MediaQuery.of(context).size.width *
+                                            0.23) -
+                                        132)
+                                    .abs(),
+                                color: Colors.yellow,
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        const Text("23% of 100%"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Continue learning",
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            const Icon(Icons.chevron_right),
+                          ],
+                        )
+                      ],
+                    )
                   ],
-                )
-              ],
-            )
+                ),
+              ),
+            ),
           ],
         ),
       ),
