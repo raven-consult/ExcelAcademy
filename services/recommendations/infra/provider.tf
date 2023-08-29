7acd7c74ab5f37e00d7e9a0f3fca1aab8efd04b4
Damian <damiakpan@gmail.com>
2023-08-29 16:28:15 +0100

    Updated recommendations provider.tf file (#136)

--- services/recommendations/infra/provider.tf
+++ services/recommendations/infra/provider.tf
@@ -1,11 +1,17 @@
-// Configure the Google Cloud provider
-provider "google" {
-  project = "excel-academy-online"
-}
-
 terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.79.0"
+    }
+  }
+
   backend "gcs" {
     bucket = "excel-academy-tfstate"
     prefix = "terraform/services/recommendations/state"
   }
+}
+
+provider "google" {
+  project = "excel-academy-online"
 }
