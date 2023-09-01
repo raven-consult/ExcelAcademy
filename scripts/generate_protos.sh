810aa1eb0bbf4522d678c11328ebabc71cc970b5
Damian Akpan <damiakpan@gmail.com>
2023-09-01 19:28:15 +0100

    Fixed issue with mobile build

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -3,7 +3,7 @@
 mkdir -p lib/services/generated
 protoc -I. \
   --dart_out=grpc:lib/services/generated/ \
-  --go_out=. --go_opt=paths=source_relative \
-  --go-grpc_out=. --go-grpc_opt=paths=source_relative \
   services/search/search.proto \
   services/recommendations/recommendations.proto
+  # --go_out=. --go_opt=paths=source_relative \
+  # --go-grpc_out=. --go-grpc_opt=paths=source_relative \
