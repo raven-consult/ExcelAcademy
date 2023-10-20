253a13b97a01f18c4be853c17ec7edd2799bab34
Josephtobi <josephtobi53@gmail.com>
2023-10-20 21:57:33 +0100

    wip

--- lib/pages/notification/notification.dart
+++ lib/pages/notification/notification.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class notificationUi extends StatelessWidget {
@@ -6,6 +7,26 @@ class notificationUi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 23),
+            child: Icon(
+              Icons.shopping_bag,
+              shadows: [
+                Shadow(
+                  color: Colors.grey[400]!,
+                  blurRadius: 3,
+                  offset: const Offset(0, 2.5),
+                ),
+              ],
+              size: 33,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
